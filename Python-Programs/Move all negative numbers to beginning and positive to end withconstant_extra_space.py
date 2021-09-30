88b2bc684df87d0b8593817b8e471c0af06b3ca6
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2021-10-01 00:44:13 +0530

    Add files via upload

---
+++ Python-Programs/Move all negative numbers to beginning and positive to end withconstant_extra_space.py
@@ -0,0 +1,13 @@
+def rearrange(a):
+    negative=0
+    positive=0
+    while positive<len(a):
+        if a[positive]<0:
+            a[positive],a[negative]=a[negative],a[positive]
+            positive+=1
+            negative+=1
+        else:
+            positive+=1
+    return a
+arr = [-2, -3, 4, 5, 6, -7, 8, 9]
+print(rearrange(arr))
---
+++ Python-Programs/Reverse_array.py
@@ -0,0 +1,10 @@
+def reverse_array(arr):
+    start=0
+    end=len(arr)-1
+    while start<end:
+        arr[start],arr[end]=arr[end],arr[start]
+        start+=1
+        end-=1
+    return arr
+arr=[1,2,3,4,5,6]
+print(reverse_array(arr))
