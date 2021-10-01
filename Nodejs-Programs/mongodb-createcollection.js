9638a38b1f4f3726b2c08428fe8234c61c730bd0
Dibash Thapa <47865470+dibashthapa@users.noreply.github.com>
2021-10-02 01:17:24 +0545

    Update mongodb-createcollection.js

--- Nodejs-Programs/mongodb-createcollection.js
+++ Nodejs-Programs/mongodb-createcollection.js
@@ -1,7 +1,7 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+const mongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, db) {
+mongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
   dbo.createCollection("customers", function(err, res) {
