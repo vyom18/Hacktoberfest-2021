94c27e08bce9c9cd03da4538dde3e8a37284a7d6
smritineupane <91712236+smritineupane@users.noreply.github.com>
2021-10-01 09:30:16 +0545

    Added positive-negative.py to Python-Programs

---
+++ Python-Programs/positive-negative.py
@@ -0,0 +1,9 @@
+# Python program to check whether a number is postive or negative
+
+num = float(input("Enter a number: "))
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
