387052e18acfccb0270459b63e8810e980a6771c
samirthapa <thapa.qw12@gmail.com>
2021-10-01 19:22:30 +0545

    Added function
    Added function which makes the code more reusable and changes the variable name which makes more sense

--- C-Programs/Palindrome.c
+++ C-Programs/Palindrome.c
@@ -1,20 +1,32 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int a,b,c, d;
-    printf("Enter a number ");
-    scanf("%d",&a);
-    b = a;
-    d=0;
-    while (b > 0) {
-    c = b % 10;
-    d = d * 10 + c;
-    b = b/10;  
-    } ;
-    if ( a == d){
-        printf("%d is palindrome",d);
-    }else{
-        printf("%d is not palindrome",a);
-    }
-    return 0;
+int checkPalindrome(int number)
+{
+  int temp, remainder, rev=0;
+  temp = number;
+    
+ while( number!=0 )
+  {
+     remainder = number % 10;
+     rev = rev*10 + remainder;
+     number /= 10;
+  }
+
+  if ( rev == temp ) return 0;
+  else return 1;
+}
+
+int main()
+{
+  int number;
+
+  printf("Enter the number: ");
+  scanf("%d", &number);
+
+  if(checkPalindrome(number) == 0)
+  printf("%d is a palindrome number.\n",number);
+  else
+  printf("%d is not a palindrome number.\n",number);
+
+  return 0;
 }
