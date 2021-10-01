7ee82ba212d87732aeddcdc958aa38a634146bdc
Susil Kumar Behera <susilbehera486@gmail.com>
2021-10-01 15:11:58 +0530

    buyingLunch file
    It's like a game. Some random people are sitting in a restaurant and one of them has to order food for all. But they can't decide who is that person. So they wrote a program in which a function will be there and it takes name of all the persons. and a random person's name will be generated and that person has to buy food.

---
+++ JavaScript-Programs/buyingLunch.js
@@ -0,0 +1,13 @@
+function whosPaying(names) {
+    
+
+    var length = arguments.length;
+    var rand = Math.random();
+    rand = rand*length;
+    var round = Math.floor(rand)+1;
+    var s = arguments[round] + " is going to buy lunch today.";
+    return s;
+    console.log(round);
+       
+}
+whosPaying("a", "below", "up", "doen", "boart");
