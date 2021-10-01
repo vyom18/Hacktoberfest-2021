9ab610da7cc9c1be7e587c3d79b6cfcf50ec4025
Aron-R <aronritesh223@gmail.com>
2021-10-01 22:23:43 +0530

    Ceaser cypher encryption in python

---
+++ Python-Programs/ceaser_cypher_encryption.py
@@ -0,0 +1,8 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet = list(alphabet)
+n = input("Enter the sentence to be encrypted ").lower().split(" ")
+s = int(input("Enter the number to be shifted by "))
+shifted_alphabet = alphabet[s:] + alphabet[:s]
+coded = [shifted_alphabet[alphabet.index(j)] for i in n for j in i]
+print("".join(coded))
+
