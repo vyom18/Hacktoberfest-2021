431908b32a58a693200fbedb1b7cdb8f52e1e752
B0s0 <80257174+B0s0@users.noreply.github.com>
2021-10-01 18:23:35 +0545

    Update neuralxor.js

--- JavaScript-Programs/neuralxor.js
+++ JavaScript-Programs/neuralxor.js
@@ -3,7 +3,7 @@
 
 const brain = require('brain.js')
 
-const net = new brain.NeuralNetwork({hiddenLayers: [3])
+const net = new brain.NeuralNetwork({hiddenLayers: [3]})
 
 const trainingData = [
   { input: [0, 0], output: [0] },
@@ -12,9 +12,7 @@ const trainingData = [
   { input: [1, 1], output: [0] }
 ];
 
-net.train(trainingData, {
-  log: (error) => console.log(error)
-});
+net.train(trainingData)
 
 console.log(net.run([0,0]));
 console.log(net.run([0,1]));
