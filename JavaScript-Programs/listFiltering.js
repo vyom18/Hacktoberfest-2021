3a73a75e0942cae9aa418b85d49c481b5b273f0a
Grishmin karki <grishmink@gmail.com>
2021-10-01 23:04:01 +0545

    delete listFiltering.js file

--- JavaScript-Programs/listFiltering.js
+++
@@ -1,8 +0,0 @@
-// codewars challenge
-// In this kata you will create a function that takes a list of
-// non - negative integers and strings and returns a new list with the strings filtered out.
-
-function filter_list(l) {
-	// Return a new array with the strings filtered out
-	return l.filter((elem) => typeof elem === "number");
-}
