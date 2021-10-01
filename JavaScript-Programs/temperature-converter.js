5b2c0478861663bb48af469da5c9a6320eb14109
Dibash Thapa <47865470+dibashthapa@users.noreply.github.com>
2021-10-02 00:56:28 +0545

    chores: changed from `var` to `let`

--- JavaScript-Programs/temperature-converter.js
+++ JavaScript-Programs/temperature-converter.js
@@ -1,16 +1,16 @@
-function cToF(celsius) 
+function celcToFare(celsius) 
 {
-  var cTemp = celsius;
-  var cToFahr = cTemp * 9 / 5 + 32;
-  var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+  const cTemp = celsius;
+  const cToFahr = cTemp * 9 / 5 + 32;
+  const message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
     console.log(message);
 }
 
-function fToC(fahrenheit) 
+function farenToCelc(fahrenheit) 
 {
-  var fTemp = fahrenheit;
-  var fToCel = (fTemp - 32) * 5 / 9;
-  var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+  const fTemp = fahrenheit;
+  const fToCel = (fTemp - 32) * 5 / 9;
+  const message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
     console.log(message);
 } 
 cToF(60);
