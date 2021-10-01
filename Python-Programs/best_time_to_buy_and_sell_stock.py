6abc4140db5b97a000097dc4e1949a10b6737da4
Akshansh Jain <jnakshansh@gmail.com>
2021-10-01 09:13:33 +0530

    Best Time to buy and sell stock.
    Stock price is given in a list like [7,1,5,3,6,4]
    Find best time to buy and then sell the stock

---
+++ Python-Programs/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,14 @@
+def maxProfit(self, prices: List[int]) -> int:
+    if len(prices) == 0: 
+        return 0        
+    else:
+        low = 99999 # represent minimum price so far
+        profitmax = 0
+        for price in prices:
+            if price > low:
+                if price - low > profitmax:
+                    profitmax = price - low                        
+            elif price < low:
+                low = price
+
+        return profitmax
