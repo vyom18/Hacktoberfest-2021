260bc336e0e93a0a83e6d6d918bfff24818e232a
smritineupane <91712236+smritineupane@users.noreply.github.com>
2021-10-01 09:37:08 +0545

    Added sum-of-arrays.js to Javascript-Programs

---
+++ JavaScript-Programs/sum-of-arrays.js
@@ -0,0 +1,9 @@
+// Javascript program to find sum of all numbers in an array
+
+const numbers = [1, 2, 3, 4, 5]
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+console.log(sum)
