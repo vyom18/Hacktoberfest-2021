4f3fe26c08bc11439f3df28b2be0a4c780100cc5
ManishKarn56 <69583236+ManishKarn56@users.noreply.github.com>
2021-10-01 20:24:42 +0545

    Update AsyncAwait.js

--- JavaScript-Programs/AsyncAwait.js
+++ JavaScript-Programs/AsyncAwait.js
@@ -1,8 +1,13 @@
-const githubUsers = async () => {
-	let response = await fetch(`https://api.github.com/users`);
-	let user = await response.json();
+const gitHubUser = async () => {
+	try {
+	   const response = await fetch(`https://api.github.com/users`);
+	   const user = await response.json();
 
-	return user;
+ 	   return user;
+		
+	} catch (err) {
+	   console.log(err);
+	}
 };
 
-githubUsers();
+gitHubUser();
