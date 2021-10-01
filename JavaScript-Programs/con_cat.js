2faaf94d4023528e5f846f0d3bc54b84e9b1f8a4
Aashutosh Sapkota <55585284+Aashutoshbro@users.noreply.github.com>
2021-10-01 13:55:33 +0545

    Create con_cat.js
    concatenation using simple array technique, my lovely brothers and sisters names

---
+++ JavaScript-Programs/con_cat.js
@@ -0,0 +1,5 @@
+const sisters = ["Anushka ", "Aaryashila ", "Archana "];
+const brothers = ["Aashutosh ", "Aaditya ", "Anuj ", "Aaryasiddhi ", "Aryan ", "Aavash ", "Aadarsh " ];
+
+const family = sisters.concat(brothers); 
+document.write(family);
