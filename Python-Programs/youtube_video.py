fe16d50512dc46ffe584e4afe0facd3b5db497e5
Gunjan Chimariya <89563859+ExtraOrdinaire1@users.noreply.github.com>
2021-10-01 12:15:22 +0545

    Update youtube_video.py

--- Python-Programs/youtube_video.py
+++ Python-Programs/youtube_video.py
@@ -3,8 +3,10 @@
 
 
 import pytube  
-from pytube import YouTube  
-video_url = 'https://www.youtube.com/watch?v=yDsMZn3olFw'   
+from pytube import YouTube 
+
+#video_url = 'https://www.youtube.com/watch?v=yDsMZn3olFw'  
+video_url = input("PASTE youtube Video Link here\n :")
 youtube = pytube.YouTube(video_url)  
 video = youtube.streams.first()  
 video.download('C:/Users/name/Desktop/')
