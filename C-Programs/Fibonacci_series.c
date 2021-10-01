b3bbd80ce02de9a0ee60a7e049ccfb705ad55862
MeetVaishnav23 <79635039+MeetVaishnav23@users.noreply.github.com>
2021-10-01 15:24:28 +0530

    Update Fibonacci_series.c

--- C-Programs/Fibonacci_series.c
+++ C-Programs/Fibonacci_series.c
@@ -3,20 +3,20 @@ int main() {
 
   int i, n;
   int n1 = 0, n2 = 1;
-  int nxtNumber = n1 + n2;
+  int nxtNumber = 0;
+  
   printf("Enter the number of terms: ");
   scanf("%d", &n);
 
-  printf("Fibonacci Series: %d, %d, ", n1, n2);
-
-  for (i = 3; i <= n; ++i) {
+  for (i = 0; i <= n; i++)
+  {
     printf("%d, ", nxtNumber);
     n1 = n2;
     n2 = nxtNumber;
     nxtNumber = n1 + n2;
   }
    printf("%d.",nxtNumber);
-   printf("\nThank you for your trying our feature.");
+   printf("\n\nThank you for your trying our feature.");
  
 
   return 0;
