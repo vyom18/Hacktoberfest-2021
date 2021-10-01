34dbd4ff46ae47852c2dce581a53c566d9109fbc
Dibash Thapa <47865470+dibashthapa@users.noreply.github.com>
2021-10-01 19:36:55 +0545

    chores: fixed unnecessary variable names

--- Python-Programs/reverse.py
+++ Python-Programs/reverse.py
@@ -1,12 +1,12 @@
 #Python program to reverse a number using function
-def reverse_Num(Number):  #Define a function
-    Reverse=0             #declarae and initialize variable Reserve
-    while(Number>0):
-        Reminder=Number %10
-        Reverse=(Reverse *10)+Reminder
-        Number=Number//10;
-    return Reverse    #return reverse value to function
-Number=int(input("Please enter a number: "))
-Reverse=reverse_Num(Number)#Call the function
-print("The entered number is: %d" %Number)
-print("The reverse number is: %d" %Reverse)
+def reverse_num(number):  #Define a function
+    reverse=0             #declarae and initialize variable Reserve
+    while(number>0):
+        reminder=number %10
+        reverse=(reverse *10)+reminder
+        number=number//10;
+    return reverse    #return reverse value to function
+number=int(input("Please enter a number: "))
+reverse=reverse_Num(number)#Call the function
+print("The entered number is: %d" %number)
+print("The reverse number is: %d" %reverse)
