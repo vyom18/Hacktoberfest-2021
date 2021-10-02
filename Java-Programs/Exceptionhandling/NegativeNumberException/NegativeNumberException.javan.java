88702b39819ac775d39470a568ebebedeab7d556
kandel@207 <62611761+Roshankandel@users.noreply.github.com>
2021-10-02 14:20:13 +0545

    Create NegativeNumberException.javan.java

---
+++ Java-Programs/Exceptionhandling/NegativeNumberException/NegativeNumberException.javan.java
@@ -0,0 +1,12 @@
+package exceptionexample3;
+
+/**
+ *
+ * @author DeLL
+ */
+public class NegativeNumberException extends Exception {
+    public NegativeNumberException(String msg){
+      super(msg);
+    }
+    
+}
