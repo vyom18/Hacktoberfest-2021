5fece5ba3abc63dd89f0f78e057835fc6e0c13b7
CaffeineDuck <samrid.pandit@gmail.com>
2021-10-02 21:06:45 +0545

    REFACTOR: Makes shit one liner

--- Python-Programs/addition.py
+++ Python-Programs/addition.py
@@ -1,14 +1,2 @@
 # Python program for addition of two numbers
-
-# function for addition
-def add(x, y):
-    return x + y
-
-
-# Main program
-
-first, second = [int(x) for x in input("Enter two value: ").split()]
-
-sum = add(first, second)
-
-print(f"The sum is {sum}")
+print(sum(map(int, input("Enter numbers seperated by space: ").split())))
