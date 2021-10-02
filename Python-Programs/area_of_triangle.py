aae15a79d03e02df43fab4bf6454d3c67233db31
Sujit Kumar Behera <64631566+s-k3278@users.noreply.github.com>
2021-10-02 13:44:19 +0530

    This ia a program to findout the area of a acircle

---
+++ Python-Programs/area_of_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+base = int(input("enter the base of triangle"))
+height = int(input("Enter the height of triangle"))
+
+area = 0.5 * base * height
+print("Are of the triangle is: ", area)
+
+
+# In[ ]:
+
+
+
+
